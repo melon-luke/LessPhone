214efee8ee7cecf0259dc77f9e67d2939a914d0a
songshenyi <songshenyi@rd.netease.com>
2022-06-03 21:30:49 +0800

    Initial Commit

---
+++ LessPhone/LessPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessPhoneApp.swift
+//  LessPhone
+//
+//  Created by 宋申易 on 2022/6/3.
+//
+
+import SwiftUI
+
+@main
+struct LessPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
